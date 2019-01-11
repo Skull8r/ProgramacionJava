254bf752259590d22dcda0be27926353ccd6bc80
fer <fer@fer-MacBook>
2019-01-11 21:08:32 +0100

    Avanzado foco evento y creada la clase FocoVentana

--- JAVA_PILDORASINFORMATICAS/src/graficos/FocoEvento.java
+++ JAVA_PILDORASINFORMATICAS/src/graficos/FocoEvento.java
@@ -1,3 +1,5 @@
+//VIDEO 74
+
 package graficos;
 
 import java.awt.Graphics;
@@ -30,15 +32,18 @@ class MarcoFoco extends JFrame{
 
 class LaminaFoco extends JPanel{
 	
+	JTextField cuadro1;
+	JTextField cuadro2;
+	
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
 		
 		setLayout(null);
 		
-		JTextField cuadro1 = new JTextField();
-		JTextField cuadro2 = new JTextField();
-		
+		cuadro1 = new JTextField();
+		cuadro2 = new JTextField();
+	
 		cuadro1.setBounds(120, 10, 150, 20);
 		cuadro2.setBounds(120, 50, 150, 20);
 		
@@ -52,17 +57,36 @@ class LaminaFoco extends JPanel{
 		cuadro2.addFocusListener(elFoco2);		
 	}
 	
-	private class LanzaFoco implements FocusListener{
+	 private class LanzaFoco implements FocusListener{
 
+		 
 		@Override
-		public void focusGained(FocusEvent arg0) {
+		public void focusGained(FocusEvent e) {
 			// TODO Auto-generated method stub			
 		}
 
 		@Override
-		public void focusLost(FocusEvent arg0) {
+		public void focusLost(FocusEvent e) {
 			// TODO Auto-generated method stub		
-			System.out.println("Has perdido el foco");			
+			cuadro1 = (JTextField) e.getSource();
+			
+			System.out.println("Foco perdido");
+			String email = cuadro1.getText();
+			boolean comprobacion = false;
+			
+			for (int i=0; i<email.length(); i++) {
+				
+				if(email.charAt(i) == '@') {
+					comprobacion = true;
+				}
+			}
+			
+			if(comprobacion) {
+				System.out.println("Es correcto");
+			}else {
+				System.out.println("Es incorrecto");
+			}
+			
 		}		
 	}	
 }
